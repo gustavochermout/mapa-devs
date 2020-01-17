80ecba9803a78ae07b76728d88b766b7aa41a8f7
DESKTOP-I6GK69U\Usuario <gustavo_chermout@hotmail.com>
2020-01-16 21:53:27 -0300

    Construção da página de cadastro de Devs

--- web/src/App.js
+++ web/src/App.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, []);
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
             <input name="github_username" id="github_username" required/>
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required/>
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
+              <input name="latitude" id="latitude" required value = {latitude}/>
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
+              <input name="longitude" id="longitude" required value = {longitude}/>
             </div>
           </div>
 
@@ -36,7 +57,55 @@ function App() {
         </form>
       </aside>
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
+              <div className="user-info">
+                <strong>Gustavo Chermout</strong>
+                <span>Nodejs, Reactjs</span>
+              </div>
+            </header>
+            <p>Desenvolvedor na alterdata</p>
+            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
+          </li>
 
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
+              <div className="user-info">
+                <strong>Gustavo Chermout</strong>
+                <span>Nodejs, Reactjs</span>
+              </div>
+            </header>
+            <p>Desenvolvedor na alterdata</p>
+            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
+              <div className="user-info">
+                <strong>Gustavo Chermout</strong>
+                <span>Nodejs, Reactjs</span>
+              </div>
+            </header>
+            <p>Desenvolvedor na alterdata</p>
+            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
+              <div className="user-info">
+                <strong>Gustavo Chermout</strong>
+                <span>Nodejs, Reactjs</span>
+              </div>
+            </header>
+            <p>Desenvolvedor na alterdata</p>
+            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
