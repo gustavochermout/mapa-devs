e40adb5c510f609e9119e8426c180d504f5ea933
DESKTOP-I6GK69U\Usuario <gustavo_chermout@hotmail.com>
2020-01-17 21:25:10 -0300

    Finalização do cadastro de devs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,7 +10,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack9@omnistack-dp3qf.mongodb.net
   useUnifiedTopology: true,
 });
 
-app.use(express.json());
+app.use(cors());
+app.use(express.json()); 
 app.use(routes);
 
 app.listen(3333);
--- web/src/App.js
+++ web/src/App.js
@@ -1,110 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
+    loadDevs();
   }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required value = {latitude}/>
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required value = {longitude}/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>        
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
-              <div className="user-info">
-                <strong>Gustavo Chermout</strong>
-                <span>Nodejs, Reactjs</span>
-              </div>
-            </header>
-            <p>Desenvolvedor na alterdata</p>
-            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
-              <div className="user-info">
-                <strong>Gustavo Chermout</strong>
-                <span>Nodejs, Reactjs</span>
-              </div>
-            </header>
-            <p>Desenvolvedor na alterdata</p>
-            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
-              <div className="user-info">
-                <strong>Gustavo Chermout</strong>
-                <span>Nodejs, Reactjs</span>
-              </div>
-            </header>
-            <p>Desenvolvedor na alterdata</p>
-            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/23001482?s=460&v=4" alt="Gustavo Chermout"/>
-              <div className="user-info">
-                <strong>Gustavo Chermout</strong>
-                <span>Nodejs, Reactjs</span>
-              </div>
-            </header>
-            <p>Desenvolvedor na alterdata</p>
-            <a href="https://github.com/gustavochermout">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
